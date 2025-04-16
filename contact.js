feada9a1f66c6332b4c0c88f1100fd6c3479974d
Eniff123 <vatpato123@gmail.com>
2025-04-17 01:40:58 +0400

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,15 @@
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (!name || !email || !password) {
+      alert("Please fill in all fields.");
+      return false;
+    }
+
+    return true;
+  }
+
+
+
